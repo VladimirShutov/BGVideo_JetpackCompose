35d5470bca22b35e046b15e89e4eab2846ec408d
shutov.vu <v.shutov2010@gmail.com>
2023-03-16 17:28:32 +0300

    img added

--- app/src/main/java/com/example/bgvideo_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/bgvideo_jetpackcompose/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.bgvideo_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,23 +14,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bgvideo_jetpackcompose.ui.theme.BGVideo_JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Cyan).fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.Top
-            ) {
-                Text(text = "Безопасный Город".uppercase(), fontWeight = FontWeight.Bold)
+            HeadImage(textHeader = "БЕЗОПАСНЫЙ ГОРОД")
+        }
+    }
+}
+@Composable
+private fun HeadImage(textHeader: String){
+    Row(
+        modifier = Modifier
+            .background(Color.Gray)
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(),
+            elevation = 5.dp
+        ) {
+            Box() {
+                Image(
+                    painter = painterResource(id = R.drawable.header_bg),
+                    contentDescription = "header image",
+                    contentScale = ContentScale.Inside
+                )
+                Column(
 
+                ) {
+                    Text(text = textHeader)
+                }
             }
         }
     }
